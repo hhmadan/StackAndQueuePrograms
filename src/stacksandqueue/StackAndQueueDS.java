877dfd10194338d082e31015ffd1575bcc9008cf
hhmadan <hhmadan03@gmail.com>
2022-11-02 21:29:24 +0530

    ADDED: UC1_Create a Stack

--- src/stacksandqueue/StackAndQueueDS.java
+++ src/stacksandqueue/StackAndQueueDS.java
@@ -1,7 +1,45 @@
 package stacksandqueue;
 
 public class StackAndQueueDS {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    private Node top;
+    public StackAndQueueDS(){
+        this.top = null;
+    }
+    public void push(int data){
+        Node node = new Node(data);
+        if(top==null){
+            node.next = null;
+        }
+        else {
+            node.next = top;
+        }
+        top = node;
+    }
+
+    public void getDisplay(){
+        Node tmp = top;
+        while(tmp != null)
+        {
+            System.out.print(tmp.data+" ");
+            tmp = tmp.next;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("WELCOME TO STACK AND QUEUE PROGRAMS");
+        StackAndQueueDS st = new StackAndQueueDS();
+        st.push(70);
+        st.push(30);
+        st.push(56);
+
+        st.getDisplay();
+
     }
 }
